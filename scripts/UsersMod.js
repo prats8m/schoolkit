d864b610a978bcd799926eabf9622a6d98eb5522
promiseverma <promise.verma@lptpl.com>
2017-06-16 18:22:34 +0530

    Added bug fixes for usermod

--- scripts/UsersMod.js
+++ scripts/UsersMod.js
@@ -899,7 +899,8 @@ app
                     $scope.editUser.last_name = angular.copy($scope.userData.user_last_name);
                     $scope.editUser.address = angular.copy($scope.userData.user_address);
                     $scope.editUser.email = angular.copy($scope.userData.user_email);
-                    $scope.editUser.expirationdate = angular.copy(new Date($scope.userData.user_expiration_date * 1000));
+                    var date = angular.copy(new Date($scope.userData.user_expiration_date * 1000));
+                    $scope.editUser.expirationdate = (date.getUTCDate() + '/' + (date.getUTCMonth() + 1) + '/' +  date.getUTCFullYear());
                     $scope.editUser.status = angular.copy($scope.userData.user_status);
                     $scope.editUser.user_name_on_lcd = angular.copy($scope.userData.user_name_on_lcd);
                     // $scope.editAccess.access_code = angular.copy($scope.userData.access_code);
