75c2a3591cfc0c52f86a99e0c052b9b6e63887f7
promiseverma <promise.verma@lptpl.com>
2017-07-11 10:11:53 +0530

    Config changes

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
-// app.constant('baseURL', 'http://35.162.244.123:8080/');  // Development Server IP
+app.constant('baseURL', 'http://35.162.244.123:8080/');  // Development Server IP
 //app.constant('baseURL', 'http://127.0.0.1:8080/');  // local  Server IP
 
-app.constant('baseURL', 'http://35.160.142.158:8080/');  //Test Server IP
+// app.constant('baseURL', 'http://35.160.142.158:8080/');  //Test Server IP
 
 //app.constant('baseURL', 'http://192.168.11.147:8080/');  //Prateek's Local Server IP
 //  app.constant('baseURL', 'http://34.208.197.185:8080/');  //client Server IP
