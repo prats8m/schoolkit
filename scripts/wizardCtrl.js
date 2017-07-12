3fee66777c8dfe9f799fa13e8182d757e050184d
Pankaj <pankaj.kumar@vvdntech.in>
2017-07-12 11:03:29 +0530

    bugs

--- scripts/wizardCtrl.js
+++ scripts/wizardCtrl.js
@@ -1,15 +1,15 @@
 app.controller('appCtrl', function ($scope, $cookies) {
     $scope.checkForWizard = function () {
-        if (!$cookies.get("isWizardUsed")) {
+        if ($cookies.get("isWizardUsed")) {
             $scope.showWizard = true;
-           // $(header).css("z-index", "0");
+            // $(header).css("z-index", "0");
         }
     }
     $scope.checkForWizard();
     $scope.wizardCompleted = function () {
         $scope.showWizard = false;
-       // alert('hi');
-                  // $(header).css("z-index", "9");
+        // alert('hi');
+        // $(header).css("z-index", "9");
     }
 });
 app
@@ -26,7 +26,7 @@ app
 app.controller('wizardCtrl', function (WizardHandler, $scope, $mdDialog, $state, $cookies, $rootScope, $location, toaster, baseURL, appConstants, userSvc, utilitySvc, $uibModal) {
     $scope.checkIfValid = function () {
         if (!$cookies.get("isWizardUsed")) {
-           // $scope.callback();
+            $scope.callback();
         }
     }
 
