30398e03c335cf317deb5403832e68508b24e16b
Himanshu Sharma <himanshu.sharma@vvdntech.com>
2017-07-18 19:04:12 +0530

    some changes

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
-// app.constant('baseURL', 'http://35.162.244.123:8080/');  // Development Server IP
-// app.constant('siteURL', 'http://35.162.244.123/');  // Development Server IP
-app.constant('siteURL', 'http://35.160.142.158/');  // Development Server IP
+ app.constant('baseURL', 'http://35.162.244.123:8080/');  // Development Server IP
+ app.constant('siteURL', 'http://35.162.244.123/');  // Development Server IP
+//app.constant('siteURL', 'http://35.160.142.158/');  // Development Server IP
 //app.constant('baseURL', 'http://127.0.0.1:8080/');  // local  Server IP
 
-app.constant('baseURL', 'http://35.160.142.158:8080/');  //Test Server IP
+//app.constant('baseURL', 'http://35.160.142.158:8080/');  //Test Server IP
 
 //app.constant('baseURL', 'http://192.168.11.147:8080/');  //Prateek's Local Server IP
 //  app.constant('baseURL', 'http://34.208.197.185:8080/');  //client Server IP
