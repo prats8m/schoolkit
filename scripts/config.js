76c7e17ff21d3d71aa1caa0fd267065b3ef642cf
prateek singh <prateek.singh@vvdntech.com>
2017-08-18 12:32:39 +0530

    clean

--- scripts/config.js
+++
@@ -1,5 +0,0 @@
-app.constant('baseURL', 'http://35.162.244.123:8080/');  // Development Server IP
-app.constant('siteURL', 'http://35.162.244.123/');  // Development Server IP
-// app.constant('siteURL', 'http://35.160.142.158/');  // Development Server IP
-// app.constant('baseURL', 'http://35.160.142.158:8080/');  //Test Server IP
-//app.constant('baseURL', 'http://34.208.197.185:8080/');  //client Server IP
